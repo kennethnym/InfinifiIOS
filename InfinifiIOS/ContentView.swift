4538f2473755e8f559d5d2dce61b64d718040048
Kenneth <kennethnym@outlook.com>
2024-07-24 23:42:08 +0100

    Initial Commit

---
+++ InfinifiIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InfinifiIOS
+//
+//  Created by Kenneth on 24/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InfinifiIOS/InfinifiIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfinifiIOSApp.swift
+//  InfinifiIOS
+//
+//  Created by Kenneth on 24/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct InfinifiIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
